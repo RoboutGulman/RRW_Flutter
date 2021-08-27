cad294e8a0fc6797609d61565e8220ee8e949757
beta442 <grandoblivioniv@gmail.com>
2021-08-28 02:11:45 +0300

    фича: добавлен роут-репо

--- lib/project/data/server.dart
+++ lib/project/data/server.dart
@@ -13,7 +13,45 @@ class RateChangeRepository {
       return gameRatingFromJson(response.body);
     } else {
       throw Exception(
-        'statusCode: ${response.statusCode}, tried getGameRating');
+          'statusCode: ${response.statusCode}, tried getGameRating');
     }
+  }
+}
+
+class AvailableTeamRepository {
+  static Future<List<int>> generateAvailableTeam(int steps) async {
+    final Uri url = Uri.parse('$SERVER/game/get-team');
+
+    List<int> availableTeamId = List<int>.generate(13, (index) => index++);
+    //TODO:
+    //ипользовать заместо генерации следующее:
+    //final http.Response response = await http.get(url);
+
+    //if (response.statusCode == 200) {
+    //  return fromJson(response.body);
+    //} else {
+    //  throw Exception(
+    //    'statusCode: ${response.statusCode}, tried getGameRating');
+    //}
+
+    List<int> generate() {
+      if (steps > 13) {
+        print('Too many steps');
+        return availableTeamId;
+      } else {
+        steps = 13 - steps;
+      }
+
+      for (var i = 0; i < steps; i++) {
+        availableTeamId.shuffle();
+        availableTeamId.removeLast();
+      }
+
+      availableTeamId.sort();
+
+      return availableTeamId;
+    }
+
+    return await generate();
   }
 }
